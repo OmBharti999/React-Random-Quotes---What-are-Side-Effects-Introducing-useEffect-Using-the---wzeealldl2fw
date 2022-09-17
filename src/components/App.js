11339b568d570258e6cf8c7c00ec8055d0a69720
OmBharti999 <55656898+OmBharti999@users.noreply.github.com>
2022-09-17 19:09:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,50 @@
-import React from "react";
+import React,{ useState } from "react";
 import "../styles/App.css";
 
 var colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857"
 ];
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const fetchQ = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.content);
+        setAuthor(data.author);
+        document.getElementsByTagName("body")[0].style.backgroundColor =
+          colors[parseInt(Math.random() * 10)];
+      });
+  };
 
-    return (
-      <div id="main">
-        <div id="wrapper">
-          
+  return (
+    <div id="main">
+      <div id="wrapper">
+        <div id="quote-box">
+          <div className="quote-text">{text}</div>
+          <div className="quote-author">{author}</div>
+          <div className="buttons">
+            <button className="button" id="new-quote" onClick={fetchQ}>
+              Click
+            </button>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default App;
